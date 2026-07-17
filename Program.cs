073a49523cf387073d90cb6325395ce4529fdc4b
海蜇零 <30751915+JellyfishZero@users.noreply.github.com>
2026-07-17 23:22:05 +0800

    feat Cookie Authentication

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using ASP_FinancialProductWishList.Repositories.Implementations;
 using ASP_FinancialProductWishList.Repositories.Interfaces;
 using ASP_FinancialProductWishList.Services.Implementations;
 using ASP_FinancialProductWishList.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,27 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 
+builder.Services
+    .AddAuthentication(
+        CookieAuthenticationDefaults.AuthenticationScheme
+    )
+    .AddCookie(options =>
+    {
+        options.Cookie.Name =
+            ".ASP_FinancialProductWishList.Auth";
+
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.Cookie.SecurePolicy =
+            CookieSecurePolicy.Always;
+
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+
+        options.ExpireTimeSpan = TimeSpan.FromHours(8);
+        options.SlidingExpiration = true;
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +53,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
